18ead2e32442f27cad4e4cd5817805b72c4ae1c3
jatom-百年酒馆 <wenjun.huang80@gmail.com>
2025-03-08 23:41:45 +0800

    test

--- pythonforandroid/bootstrap.py
+++ pythonforandroid/bootstrap.py
@@ -392,6 +392,7 @@ class Bootstrap:
         info('Frying eggs in {}'.format(sitepackages))
         for d in listdir(sitepackages):
             rd = join(sitepackages, d)
+            info(' found ' + d)
             if isdir(rd) and d.endswith('.egg'):
                 info('  ' + d)
                 files = [join(rd, f) for f in listdir(rd) if f != 'EGG-INFO']
